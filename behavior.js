b7ff31cdaea23983622850f9fe24f763d43bab37
naol1 <81456232+naol1@users.noreply.github.com>
2021-06-01 15:14:28 -0700

    Add files via upload

--- behavior.js
+++ behavior.js
@@ -1,8 +1,34 @@
-var thumbnailElement=document.getElementById("smart_thumbnail");
+document.addEventListener("DOMContentLoaded", function(event) {
 
-function enlargeImg(){
-    thumbnailElement.style.transform="scale(1.5)";
-    thumbnailElement.style.transition="transform 0.25s ease";
-function resetImg(){
-    thumbnailElement.style.transform="scale(1)";
-    thumbnailElement.style.transition="transform 0.25s ease";
+  var d = document,
+  s = d.createElement('script');
+	s.src = 'https://naol1.disqus.com/embed.js';
+	s.setAttribute('data-timestamp', +new Date());
+	(d.head || d.body).appendChild(s);
+
+
+  img = document.getElementById("ULA");
+  function enlargeImg(){
+    img.style.transform="scale(1.5)";
+    img.style.transition = "transform 0.25s ease"
+  }
+
+  function resetImg(){
+
+    img.style.transform="scale(1)";
+    img.style.transition = "transform 0.2s ease";
+
+
+  }
+<div id="fb-root"></div>
+  (function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
+});
