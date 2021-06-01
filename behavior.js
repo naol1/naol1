d74661d2ecd7bafa1127f2928b6b6f321e4711cb
naol1 <81456232+naol1@users.noreply.github.com>
2021-06-01 15:40:22 -0700

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -1,22 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
-alert("Please press ok to continue");
-function change7()
-{
-  document.getElementById('imag').src="https://static1.bigstockphoto.com/5/5/1/large1500/155664566.jpg";
-
-}
-function change8()
-{
-  document.getElementById('imag').src="https://i.pinimg.com/originals/60/9f/55/609f556b75881aa1187906a35f813c03.jpg";
-
-}
-function change9()
-{
-  document.getElementById('imag').src="https://iso.500px.com/wp-content/uploads/2016/03/stock-photo-142984111.jpg";
-
-
-
   var d = document, s = d.createElement('script');
   s.src = 'https://naol1.disqus.com/embed.js';
   s.setAttribute('data-timestamp', +new Date());
